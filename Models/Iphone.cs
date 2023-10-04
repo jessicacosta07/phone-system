c4fa46887c8e6d21103ed39b3ac377380ba0e6bc
jessicacosta07 <jessikacostajf@gmail.com>
2023-10-03 22:17:33 -0300

    Implementação da lógica

---
+++ Models/Iphone.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phone_system.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// TODO: Realizar os testes com as classes Nokia e Iphone
+
+using phone_system.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Nokia n1 = new Nokia(numero: "1234", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+n1.Ligar();
+n1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone i1 = new Iphone(numero: "5678", modelo: "Modelo 2", imei: "22222222", memoria: 256);
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Slack");
